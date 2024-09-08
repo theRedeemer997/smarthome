1cd190f29b0528a68c57229c75c25c23afa7ab94
theRedeemer997 <manu71997mathew@gmail.com>
2024-09-07 23:25:05 -0400

    changes done: * partials created for navbar, header, hearosection, banner

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("node:path");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 const renderHomePage = require("./routes/renderHomePage");
