67f59ea2decdd81e9f4871065af7f3e8ddd256a8
theRedeemer997 <manu71997mathew@gmail.com>
2024-09-12 01:27:27 -0400

    Changes done: * Implemented customer feedback modal

---
+++ public/js/main.js
@@ -0,0 +1,36 @@
+const feebackModal = bootstrap.Modal.getOrCreateInstance("#exampleModal");
+console.log("🚀 ~ feebackModal:", feebackModal);
+
+window.addEventListener("DOMContentLoaded", () => {
+  if (JSON.parse(sessionStorage.getItem("feedbackModal")) !== "up") {
+    feebackModal.show();
+    if (feebackModal._isShown) {
+      sessionStorage.setItem("feedbackModal", JSON.stringify("up"));
+    }
+  }
+});
+
+const stars = document.querySelectorAll(".star");
+console.log("🚀 ~ stars:", stars);
+for (let i = 0; i < stars.length; i++) {
+  stars[i].addEventListener("click", highLightStars);
+}
+
+function highLightStars(e) {
+  removeHighlighted();
+  let id = e.target.id;
+  let number = id.split("-")[1];
+  for (let i = number; i > 0; i--) {
+    let finalID = "i-star-" + i;
+    let classList = document.getElementById(finalID).classList;
+    classList.add("high");
+  }
+}
+
+function removeHighlighted() {
+  for (let i = 1; i <= 5; i++) {
+    let finalID = "i-star-" + i;
+    let classList = document.getElementById(finalID).classList;
+    classList.remove("high");
+  }
+}
