0bfa9f6ed8bbb9f7c93f535fe3c3a00728589a79
theRedeemer997 <manu71997mathew@gmail.com>
2024-09-14 18:36:02 -0400

    Changes done: * Feedback and testimonials are implemented

--- routes/renderHomePage.js
+++ routes/renderHomePage.js
@@ -2,8 +2,21 @@ const userFeedback = require("../model/userFeedback");
 
 const renderHomePage = async (req, res) => {
   const feedback = await userFeedback.find();
+  let sumOfAllFeedbacks = 0;
+  for (let i = 0; i < feedback.length; i++) {
+    sumOfAllFeedbacks = sumOfAllFeedbacks + feedback[i].Rating;
+  }
+  let avgOfAllFeedbacks = Number(
+    Math.ceil(sumOfAllFeedbacks / feedback.length)
+  );
+
+  let noOfCustomer = feedback.length;
+  console.log("🚀 ~ renderHomePage ~ noOfCustomer:", noOfCustomer);
+  console.log("🚀 ~ renderHomePage ~ avgOfAllFeedbacks:", avgOfAllFeedbacks);
+  console.log("🚀 ~ renderHomePage ~ sumOfAllFeedbacks:", sumOfAllFeedbacks);
+
   // console.log(feedback[0].Name);
-  res.render("index", { feedback });
+  res.render("index", { feedback, avgOfAllFeedbacks, noOfCustomer });
 };
 
 module.exports = renderHomePage;
